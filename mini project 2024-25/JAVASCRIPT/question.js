8aab61d8048b1ad413c4ed96d02b7c1a02365c40
satyamsahu15 <smartsahuji1504@gmail.com>
2025-03-21 11:18:11 +0530

    Add files via upload

---
+++ mini project 2024-25/JAVASCRIPT/question.js
@@ -0,0 +1,112 @@
+let questions=[
+    {
+        numb:"1",
+        question:"What does HTML stand for? ",
+        answer:"C.Hyper Text Markup Language",
+        options:[
+            "A. Hyper Type Multi Language",
+            "B.Hyper Text Multiple Language",
+            "C.Hyper Text Markup Language",
+            "D.Home Text Multi Language"
+        ]
+    },
+    {
+        numb:"2",
+        question:"What does CSS stand for?",
+        answer:"A. Cascadding Style sheet",
+        options:[
+            "A. Cascadding Style sheet",
+            "B. Cute Style sheet",
+            "C. Computer Style sheet",
+            "A. Codehal Style sheet"
+        ]
+    },
+    {
+        numb:"3",
+        question:"What does PHP stand for?",
+        answer:"A. Hypertext Preprocesor",
+        options:[
+            "A. Hypertext Preprocesor",
+            "B. Hypertext Pregramming",
+            "C. Hometext Pregramming",
+            "D. Pregramming Hypertext Preprocesor"
+        ]
+    },
+    {
+        numb:"4",
+        question:"What does SQL stand for?",
+        answer:"A. Structured Query language",
+        options:[
+            "A. Structured Query language",
+            "B. Strength Query language",
+            "C. Scinence Question language",
+            "D. Stylesheet Query language"
+        ]
+    },
+    {
+        numb:"5",
+        question:"What does XML stand for?",
+        answer:"D. Extensible Markup Language",
+        options:[
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    },
+    {
+        numb:"6",
+        question:"Inside which HTML element do we put the JavaScript?",
+        answer:"A. script",
+        options:[
+            "A. script",
+            "B. javascript",
+            "C. scripting",
+            "D. js"
+        ]
+    },
+    {
+        numb:"7",
+        question:"Where is the correct place to insert a JavaScript?",
+        answer:"A. Both the head section and the body section are correct",
+        options:[
+            "A. Both the head section and the body section are correct",
+            "B. The head section",
+            "C. The body section",
+            "D. None of above"
+        ]
+    },
+    {
+        numb:"8",
+        question:"HELO and PORT, respectively, are commands from the protocols  ",
+        answer:"B. SMTP and FTP",
+        options:[
+            "A.TELNET and POP3 ",
+            "B. SMTP and FTP",
+            "C. FTP and HTTP",
+            "D. HTTP and TELNET"
+        ]
+    },
+    {
+        numb:"9",
+        question:"Which of the following is TRUE only of XML but NOT HTML?",
+        answer:"D. It allows user defined tags",
+        options:[
+            "B. It is derived from SGML",
+            "C. It describes content and layout",
+            "A. It is restricted only to be used with web browsers",
+            "D. It allows user defined tags"
+        ]
+    },
+    {
+        numb:"10",
+        question:"Which of the below is the correct syntax to put a line over text in CSS?",
+        answer:"C. text-decoration: overline",
+        options:[
+            "A. text-decoration: line",
+            "B. text-decoration: none",
+            "C. text-decoration: overline",
+            "D. text-decoration: underline"
+        ]
+    },
+];
